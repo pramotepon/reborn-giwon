e7505a5ecb161335c250fc101344c7a5b57aea9a
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-05-10 14:25:02 +0700

    Fix Dashboard screen navbar and user profile Fixed position

--- client/src/layout/DashboardLayout/Dashboard.jsx
+++ client/src/layout/DashboardLayout/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import GoalProgress from "../../components/GoalProgress";
 import Navbar from "../../components/Navbar";
 import UserProfile from "../../components/UserProfile";
+import "../../assets/css/DashboardScreen.css";
 
 const Dashboard = ({ children }) => {
 	return (
@@ -9,12 +10,25 @@ const Dashboard = ({ children }) => {
 			<Navbar />
 
 			<div className="row">
-				<div className="userAndGoal col-3">
-					<UserProfile />
-					<GoalProgress />
+				<div className="userAndGoal col-3 mt-5 pt-4">
+					<div className="position-fixed container-user-profile">
+						<UserProfile />
+						<GoalProgress />
+					</div>
 				</div>
 
-				<div className="rightContainer col-9">{children}</div>
+				{/* <div className="rightContainer col-9">{children}</div> */}
+				<div className="rightContainer col-9 mt-5 pt-5">
+					<div className="min-vh-100">
+						<h1>test</h1>
+					</div>
+					<div className="min-vh-100 bg-success">
+						<h1>test</h1>
+					</div>
+					<div className="min-vh-100 bg-danger">
+						<h1>test</h1>
+					</div>
+				</div>
 			</div>
 			<br></br>
 		</div>
