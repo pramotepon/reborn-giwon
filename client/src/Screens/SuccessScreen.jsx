203006e7aa785b5ab8ab4cf224b850009c1b1c05
ppongpeera <ppongpeera@gmail.com>
2023-05-10 20:23:23 +0700

    Vee first commit

---
+++ client/src/Screens/SuccessScreen.jsx
@@ -0,0 +1,12 @@
+import Success from "../components/Success";
+import Dashboard from "../layout/DashboardLayout/Dashboard";
+const SuccessScreen = () => {
+	return (
+		<Dashboard>
+			<Success />
+		</Dashboard>
+	);
+};
+
+
+export default SuccessScreen;
