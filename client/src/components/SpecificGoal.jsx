dad2505131ef23815878114eb3360e17646c0d5f
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-05-11 11:13:16 +0700

    Fix React code

--- client/src/components/SpecificGoal.jsx
+++ client/src/components/SpecificGoal.jsx
@@ -4,57 +4,60 @@ import "../assets/css/components/SpecificGoal.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SpecificGoal = () => {
-	const [weight, setWeight] = useState(0);
-      
-        const handleIncrement = () => {
-          setWeight(weight + 1);
-        };
-      
-        const handleDecrement = () => {
-          if (weight > 0) {
-            setWeight(weight - 1);
-          }
-        };
-      
-        const handleInputChange = (event) => {
-          const value = event.target.value;
-          if (value >= 0) {
-            setWeight(value);
-          }
-        };
+  const [weight, setWeight] = useState(0);
+  
+  const intWeight = parseInt(weight);
+
+  const handleIncrement = () => {
+
+    setWeight(intWeight + 1);
+  };
+
+  const handleDecrement = () => {
+    if (intWeight > 0) {
+      setWeight(intWeight - 1);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    if (value >= 0) {
+      setWeight(value);
+    }
+  };
 
 
 
   return (
     <div className='weightContainer'>
-        
-        <label className='setGoal'>Set Your Weight Goal (kg):</label>
 
-        <div className='weightGoal'>
-        
-        
-            <div className="input-stepper">
-                <input
-                className='input-weight'
-                type="number"
-                id="weight-input"
-                value={weight}
-                onChange={handleInputChange}
-                />
+      <label className='setGoal'>Set Your Weight Goal (kg):</label>
 
-                <div className="stepper">
-                    
-                    <FontAwesomeIcon icon="fa-solid fa-angle-up" onClick={handleIncrement} className='pointer' />
+      <div className='weightGoal'>
 
-                    <FontAwesomeIcon icon="fa-solid fa-angle-down" onClick={handleDecrement} className='pointer' />
-                
-                </div>
 
-            </div>
-            
-            <button className='done'>Done</button>
-        
+        <div className="input-stepper">
+          <input
+            className='input-weight'
+            type="number"
+            id="weight-input"
+            value={weight}
+            onChange={handleInputChange}
+          />
+
+          <div className="stepper">
+
+            <FontAwesomeIcon icon="fa-solid fa-angle-up" onClick={handleIncrement} className='pointer' />
+
+            <FontAwesomeIcon icon="fa-solid fa-angle-down" onClick={handleDecrement} className='pointer' />
+
+          </div>
+
         </div>
+
+        <button className='done'>Done</button>
+
+      </div>
 
     </div>
 
