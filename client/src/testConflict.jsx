179e57459041f2981a7d165899908a612e15327f
BoomWSL <boom@live.se>
2023-05-11 11:38:01 +0700

    conflict edit test

--- client/src/testConflict.jsx
+++ client/src/testConflict.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const testConflict = () => {
-  return (
-    <div>
-        <p>Plugsza</p>
-    </div>
-  )
-}
+	return (
+		<div>
+			<h1>Test Conflict</h1>
+			<p>
+				lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+				voluptatum. Quisquam, voluptatum. Quisquam, voluptatum.
+			</p>
+		</div>
+	);
+};
 
-export default testConflict
+export default testConflict;
