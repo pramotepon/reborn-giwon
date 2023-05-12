badb01ea8347a2a3b269c9068f5bf999f80da12c
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-05-12 14:08:48 +0700

    done

--- server/controllers/toey/activityEditController.js
+++ server/controllers/toey/activityEditController.js
@@ -1,8 +1,25 @@
-import User from '../../models/User.js';
+import Activity from '../../models/Activity.js';
 
-const activityUpdate = (req, res) => {
-    res.json('Hello from register');
-}
+const activityUpdate = async (req, res) => {
+    // res.json('Hello from register');
+    const { activity_name, activity_type, calendar, duration, description, image } = req.body;
+    const activityId = req.params.id;
+
+    try {
+        const activity = await Activity.findOneAndUpdate(
+          { _id: activityId },
+          { activity_name, activity_type, calendar, duration, description, image },
+          { new: true }
+        );
+        if (!activity) {
+          return res.status(404).json({ message: 'Activity not found' });
+        }
+        res.json(activity);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+      }
+};
 
 const activityEditController = {
     activityUpdate: activityUpdate
