977897bb83aff14998786fab1f97733cae58845e
Sasiwan Janma <sasiwanj16@gmail.com>
2023-05-14 00:56:54 +0700

    added bcrypt hash password

--- server/controllers/pang/userResetPassword.js
+++ server/controllers/pang/userResetPassword.js
@@ -1,17 +1,20 @@
 import User from "../../models/User.js";
+import bcrypt from "bcryptjs";
 
+const bcryptSalt = bcrypt.genSaltSync(10);
 const resetPassword = async (req, res) => {
     let body = req.body;
     const filter = { email: body.email};
-    const update = { password: body.password };
+    const update = { password: bcrypt.hashSync(body.password, bcryptSalt) };
 
     let user = await User.findOneAndUpdate(filter, update);
-    if (!user) {
-        res.status(401).json({"status": "error", "message" : "Email mismatch"})
-    }
-    else {
+    if (user) {
         res.json({"status": "success", "message" : "Password reset successfully"})
     }
+    else {
+        res.status(401).json({"status": "error", "message" : "Email mismatch"})
+    }
+}
 
 const userResetPassword = {
     resetPassword: resetPassword
