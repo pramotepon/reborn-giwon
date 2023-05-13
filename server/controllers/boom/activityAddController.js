6faea4bf40a635b2b1979716c1fa040819f9de6f
BoomWSL <boom@live.se>
2023-05-13 19:39:31 +0700

    Activity Add with Image Upload via Cloudinary Done

--- server/controllers/boom/activityAddController.js
+++ server/controllers/boom/activityAddController.js
@@ -1,33 +1,72 @@
+import { v2 as cloudinary } from "cloudinary";
+import * as dotenv from "dotenv";
 import Activity from "../../models/Activity.js";
 import User from "../../models/User.js";
 
-//http://localhost:8080/activities/add/:id
+dotenv.config();
 
-const addActivity = (req, res) => {
-	const {
-		user_id,
-		activity_name,
-		activity_type,
-		calendar,
-		duration,
-		description,
-		image, //PC PATH
-	} = req.body;
+// Cloudinary configuration
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_BOOM_CLOUDNAME,
+	api_key: process.env.CLOUDINARY_BOOM_API_KEY,
+	api_secret: process.env.CLOUDINARY_BOOM_API_SECRET,
+});
 
-	const newActivity = new Activity({
-		user_id,
-		activity_name,
-		activity_type,
-		calendar,
-		duration,
-		description,
-		image,
+const cloudinaryImageUpload = async (img) => {
+	if (!img) {
+		throw new Error("No image provided.");
+	}
+
+	const validFileTypes = ["jpg", "jpeg", "gif", "png"];
+	const fileType = img.split(".").pop().toLowerCase();
+
+	if (!validFileTypes.includes(fileType)) {
+		throw new Error(
+			"Invalid file type. Only JPG, JPEG, GIF, and PNG files are allowed."
+		);
+	}
+
+	const response = await cloudinary.uploader.upload(img, {
+		height: 150,
+		width: 150,
+		crop: "fill",
 	});
 
-	newActivity
-		.save()
-		.then(() => res.json("Activity added!"))
-		.catch((err) => res.status(400).json({ error: "Something Went Wrong" }));
+	return response.url;
+};
+
+const addActivity = async (req, res) => {
+	let {
+		user_id,
+		activity_name,
+		activity_type,
+		calendar,
+		duration,
+		description,
+		image, // PC PATH
+	} = req.body;
+
+	try {
+		let imageCloudUrl = null;
+		if (image) {
+			imageCloudUrl = await cloudinaryImageUpload(image);
+		}
+
+		const newActivity = new Activity({
+			user_id,
+			activity_name,
+			activity_type,
+			calendar,
+			duration,
+			description,
+			image: imageCloudUrl,
+		});
+
+		await newActivity.save();
+		res.json("Activity added!");
+	} catch (error) {
+		res.status(400).json("Error: " + error);
+	}
 };
 
 const activityAddController = {
@@ -35,15 +74,3 @@ const activityAddController = {
 };
 
 export default activityAddController;
-
-// here is my example of how to add a new activity in postman
-// {
-//     "user_id": "60b9b0b3b3b3b3b3b3b3b3b3",
-
-//     "activity_name": "test",
-//     "activity_type": "test",
-//     "calendar": "test",
-//     "duration": "test",
-//     "description": "test",
-//     "image": "test"
-// }
