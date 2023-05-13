e2c4dd406e621ccb8171b3f24061dabf51f9b819
Sasiwan Janma <sasiwanj16@gmail.com>
2023-05-13 17:33:05 +0700

    API verify reset password

--- server/controllers/pang/userVerifyResetPasswordController.js
+++ server/controllers/pang/userVerifyResetPasswordController.js
@@ -1,7 +1,27 @@
 import User from '../../models/User.js';
+import { ObjectId } from 'mongoose';
 
-const verifyResetPassword = (req, res) => {
-    res.json('Hello from login');
+const verifyResetPassword = async (req, res) => {
+    let body = req.body;
+    let id = req.params.id;
+    let user = await User.findById(id);
+    if (body.email === user.email && body.height === user.height 
+        && body.weight === user.weight && body.gender === user.gender) {
+        res.json(
+            {
+                "status": "success",
+                "message" : "You can reset password"
+            }
+        )
+    }
+    else {
+        res.status(401).json(
+            {
+                "status": "error",
+                "message" : "You can't reset password"
+            }
+        )
+    }
 }
 
 const userVerifyResetPasswordController = {
