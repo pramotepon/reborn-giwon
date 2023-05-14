a09309e286dbb989167ec3257c1241462dc042b8
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-05-14 22:08:16 +0700

    add function delete before do anything

--- server/controllers/toey/activityEditController.js
+++ server/controllers/toey/activityEditController.js
@@ -1,4 +1,28 @@
 import Activity from '../../models/Activity.js';
+import { v2 as cloudinary } from "cloudinary";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+// Cloudinary configuration
+cloudinary.config({
+	cloud_name: process.env.TOEY_CLOUD_NAME,
+	api_key: process.env.TOEY_API_KEY,
+	api_secret: process.env.TOEY_API_SECRET,
+});
+
+const deleteFile = async (publicId) => {
+	try {
+		const result = await cloudinary.uploader.destroy(publicId);
+		if (result.result === "ok") {
+			console.log(`File with public ID ${publicId} has been deleted.`);
+		} else {
+			throw new Error("Failed to delete file from Cloudinary.");
+		}
+	} catch (error) {
+		throw new Error("Error deleting file from Cloudinary: " + error);
+	}
+};
 
 const activityUpdate = async (req, res) => {
     // res.json('Hello from register');
