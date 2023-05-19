e3a98b795c15579e17332d2c3521c7b567f770b5
BoomWSL <boom@live.se>
2023-05-19 10:37:50 +0700

    Edit goalprogress weight and goal

--- client/src/layout/DashboardLayout/Dashboard.jsx
+++ client/src/layout/DashboardLayout/Dashboard.jsx
@@ -1,23 +1,23 @@
 import React, { useContext, useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
 import "../../assets/css/DashboardScreen.css";
 import GoalProgress from "../../components/GoalProgress";
 import GoalSelection from "../../components/GoalSelection";
 import Navbar from "../../components/Navbar";
 import UserProfile from "../../components/UserProfile";
 import { UserContext } from "../../contexts/UserContext";
-import { Navigate } from "react-router-dom";
 
 const Dashboard = ({ children }) => {
 	const { user } = useContext(UserContext);
 
 	let displayShow;
 	if (!user) {
-		return <Navigate to={'/login'} />
+		return <Navigate to={"/login"} />;
 	}
 
-	if(!user.goal){
+	if (!user.goal) {
 		displayShow = <GoalSelection />;
-	}else{
+	} else {
 		displayShow = children;
 	}
 
@@ -28,8 +28,17 @@ const Dashboard = ({ children }) => {
 			<div className="row mt-5 pt-5">
 				<div className="userAndGoal col-3">
 					<div className="position-fixed container-user-profile">
-						<UserProfile userId={user._id} userDisplayName={user.displayName} userWeight={user.weight} userImage={user.image} />
-						<GoalProgress userId={user._id} userGoal={user.goal} />
+						<UserProfile
+							userId={user._id}
+							userDisplayName={user.displayName}
+							userWeight={user.weight}
+							userImage={user.image}
+						/>
+						<GoalProgress
+							userId={user._id}
+							userGoal={user.goal}
+							userWeight={user.weight}
+						/>
 					</div>
 				</div>
 
