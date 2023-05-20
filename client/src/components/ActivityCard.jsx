59fb5f86679a070c0e664c99bc1f6ef79def1964
ArlifTagaree <liffism9@gmail.com>
2023-05-20 20:03:22 +0700

    Almost done Activity

--- client/src/components/ActivityCard.jsx
+++ client/src/components/ActivityCard.jsx
@@ -18,11 +18,11 @@ const ActivityCard = (props) => {
             <a href="/activitydetail" className="text-decoration-none text-dark link-primary"><strong>{props.activity.title}</strong></a>
           </h2>
           <div className="my-top-right-button">
-            <button className="btn p-0">
+            <button className="btn p-0" style={{marginRight :  "10px"}}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
+                width="32"
+                height="32"
                 fill="currentColor"
                 className="bi bi-pencil-square"
                 viewBox="0 0 16 16"
@@ -34,16 +34,24 @@ const ActivityCard = (props) => {
                 />
               </svg>
             </button>
-            <button className="btn p-0" style={{ marginLeft: "4px" }}>
-              <svg xmlns="http://www.w3.org/2000/svg" style={{ color: "red" }} width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+            <button className="btn p-0" style={{ margin: "10px" }}>
+              <svg xmlns="http://www.w3.org/2000/svg" style={{ color: "red" }} width="32" height="32" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
                 <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
               </svg>
             </button>
           </div>
-          <p style={{fontSize:"48px"}}>{props.activity.activity_name}</p>
-          <p style={{fontSize:"24px"}}>Description :{props.activity.description}</p>
-          <p>Duration {props.activity.duration.hour} hour : {props.activity.duration.minute} minute</p>
+
+          <div className="toeyja">
+          <div>
+          <p style={{fontSize:"52px"}}>{props.activity.activity_name}</p>
+          <p style={{fontSize:"22px"}}>Description :{props.activity.description}</p>
+          <p style={{fontSize:"28px"}}>Duration {props.activity.duration.hour} hour : {props.activity.duration.minute} minute</p>
+          </div>
+          <div className="toeykung">
+          <p style={{fontSize:"160px", margin:"0"}}>{props.activity.current_weight}</p><p style={{fontSize:"32px"}}>Kg.</p>
+          </div>
+          </div>
         </div>
       </div>
     </div>
