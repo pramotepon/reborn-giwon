47ae4309aa91f8a9c1e9e6a8580e63f052a30723
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-05-20 11:21:26 +0700

    Add catch user data

--- server/controllers/plug/userGetDataController.js
+++ server/controllers/plug/userGetDataController.js
@@ -8,16 +8,20 @@ dotenv.config();
 const jwtSecret = process.env.JWT_KEY;
 
 const userData = async (req, res) => {
-    const { token } = req.params;
-    if (token) {
-        // กำหนด Exp times
-        jwt.verify(token, jwtSecret, {}, async (err, user) => {
-            if (err) throw err;
-            const { displayName, weight, goal, image, _id } = await User.findOne({ _id: user._id });
-            res.json({ displayName, weight, goal, image, _id });
-        });
-    } else {
-        res.json(null);
+    try {
+        const { token } = req.params;
+        if (token) {
+            // กำหนด Exp times
+            jwt.verify(token, jwtSecret, {}, async (err, user) => {
+                if (err) {
+                    return res.status(403).json("Access denine token.")
+                };
+                const { displayName, weight, goal, image, _id } = await User.findOne({ _id: user._id });
+                res.json({ displayName, weight, goal, image, _id });
+            });
+        }
+    } catch (e) {
+        res.json(e.message);
     }
 }
 
