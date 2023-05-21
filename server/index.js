f763dd6f1e1985401ded0efafc0f6c07f466e2da
ppongpeera <ppongpeera@gmail.com>
2023-05-21 23:19:31 +0700

    before pull k'Boom code

--- server/index.js
+++ server/index.js
@@ -13,8 +13,8 @@ const IP_ADDRESS = "127.0.0.1";
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-	origin: "http://localhost:5173",
-	credentials: true,
+	origin: 'http://localhost:5173',
+	credentials: false,
 };
 
 const app = express();
