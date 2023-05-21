7565b34fccbbb900e830f806f8f4827f0655bb66
BoomWSL <boom@live.se>
2023-05-21 11:12:30 +0700

    before pull rebase

--- client/src/Screens/ActivityCardScreen.jsx
+++ client/src/Screens/ActivityCardScreen.jsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-import "chart.js/auto";
 import React, { useContext, useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import { Link } from "react-router-dom";
@@ -32,53 +31,12 @@ const ActivityCardScreen = () => {
 	useEffect(() => {
 		fetchData();
 	}, []);
-	const chartData = {
-		labels:
-			weightData && weightData.length > 0
-				? weightData.map((data) => data.date)
-				: [],
-		datasets: [
-			{
-				label: "Weight",
-				data:
-					weightData && weightData.length > 0
-						? weightData.map((data) => data.weight)
-						: [],
-				borderColor: "rgba(75, 192, 192, 1)",
-				backgroundColor: "rgba(75, 192, 192, 0.4)",
-			},
-			{
-				label: "Goal",
-				data: Array(weightData.length).fill(goalWeight),
-				borderColor: "rgba(255, 99, 132, 1)",
-				backgroundColor: "rgba(255, 99, 132, 0.4)",
-				borderDash: [5, 5],
-			},
-		],
-	};
-
-	const chartOptions = {
-		scales: {
-			y: {
-				beginAtZero: true,
-			},
-		},
-	};
 
 	return (
 		<Dashboard>
 			<div className="mb-3">
 				<ButtonNewActivity activity={activities} />
 			</div>
-			<div>
-				<h2>Weight Trend</h2>
-				{weightData ? (
-					<Line data={chartData} options={chartOptions} />
-				) : (
-					<p>No weight data available.</p>
-				)}
-			</div>
-
 			{activities.map((activity, index) => {
 				return <ActivityCard activity={activity} key={index} />;
 			})}
