76237442ba4e5a6b43d5c27f5852fc21cb6b9964
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-05-22 14:42:38 +0700

    Set error message

--- server/controllers/plug/userRegisterController.js
+++ server/controllers/plug/userRegisterController.js
@@ -18,8 +18,7 @@ const register = async (req, res) => {
 			return res.status(403).send("Email is valid");
 		}
 		// Array of allowed files
-		const array_of_allowed_files = ["png", "jpeg", "jpg", "gif"];
-
+		const array_of_allowed_files = ["png", "jpeg", "jpg", "gif", "jfif"];
 		let url_image = image;
 		let public_id_image = null;
 
@@ -33,7 +32,7 @@ const register = async (req, res) => {
 			// Get the extension of the uploaded file
 			// Check if the uploaded file is allowed
 			if (!array_of_allowed_files.includes(extImage)) {
-				throw new Error('Invalid file');
+				return res.status(415).json('Invalid file');
 			}
 			const result = await cloudinary.uploader.upload(image, {
 				height: 150,
@@ -41,7 +40,7 @@ const register = async (req, res) => {
 				crop: "fill",
 			});
 			if (!result) {
-				throw new Error("Cloud image server have a poblem.");
+				return res.status(500).json('Cloud image server have a poblem.');
 			}
 			url_image = result.url;
 			public_id_image = result.public_id;
