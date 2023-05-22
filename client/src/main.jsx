b38b6726a855cb8eab5b99cca094d21abd6d6b9b
Pramote Phanon <40195236+pramotepon@users.noreply.github.com>
2023-05-22 10:20:15 +0700

    Set server domain base.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -25,7 +25,7 @@ import { UserContextProvider } from "./contexts/UserContext.jsx";
 
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000/";
+axios.defaults.baseURL = "https://giddy-pear-jay.cyclic.app";
 axios.defaults.withCredentials = true;
 
 const router = createBrowserRouter([
