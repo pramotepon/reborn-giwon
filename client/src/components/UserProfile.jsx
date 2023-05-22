d16cbc44bf2e9163f3575d26ff145550be7c3326
BoomWSL <boom@live.se>
2023-05-23 00:14:51 +0700

    Goal Chart Update

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,62 +1,58 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useEffect , useContext, useState } from "react";
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 import "../assets/css/components/UserProfile.css";
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 
 const UserProfile = ({ userId, userDisplayName, userWeight, userImage }) => {
 	const [weight, setWeight] = useState(userWeight);
-
-	// Vee's CODE is here
-	
 	const [hasAchievedGoal, setHasAchievedGoal] = useState(false);
 	const navigate = useNavigate();
 
 	useEffect(() => {
-	  const fetchGoalAchievementStatus = async () => {
-		try {
+		const fetchGoalAchievementStatus = async () => {
+			try {
+				const response = await axios.get(`/users/goal-success/${userId}`);
+				const data = response.data;
+				setHasAchievedGoal(data);
+			} catch (error) {
+				console.error(error);
+			}
+		};
 
-	   // axios or fetch
-	  //  const response = await fetch(`/users/goal-success/${userId}`
-
-		  const response = await axios.get(`/users/goal-success/${userId}`);
-
-		//   const data = await response.json();
-		//   console.log(userId);
-		//   setHasAchievedGoal(data);
-
-		  const data = response.data;
-      	  setHasAchievedGoal(data);
-	
-		} catch (error) {
-		  console.error(error);
-		}
-	  };
-  
-	  fetchGoalAchievementStatus();
+		fetchGoalAchievementStatus();
 	}, [userId]);
-  
+
 	useEffect(() => {
-	  if (hasAchievedGoal) {
-		navigate("/success");
-	  }
+		if (hasAchievedGoal) {
+			navigate("/success");
+		}
 	}, [hasAchievedGoal, navigate]);
 
 	return (
-		<div className="userProfile">
-
-			<img src={userImage ? userImage : "http://placekitten.com/200/200"} className="img-fluid" alt="" />
+		<div className="userProfile position-relative">
+			<div className="position-absolute top-0 end-0 mt-2 me-2">
+				<Button variant="" href="/editprofile">
+					<FontAwesomeIcon
+						className="editIcon"
+						icon="fa-regular fa-pen-to-square"
+					/>
+				</Button>
+			</div>
+			<div className="text-center">
+				<div className="user-profile-image">
+					<img
+						src={userImage ? userImage : "http://placekitten.com/200/200"}
+						className="img-fluid"
+						alt=""
+					/>
+				</div>
+			</div>
 			<div className="displayName">
 				<p className="displayNameText">{userDisplayName}</p>
 			</div>
-			<p className="currentWeightText">Current Weight : {weight} kg</p>
-			<Button variant="" href="/editprofile">
-				<FontAwesomeIcon
-					className="editIcon"
-					icon="fa-regular fa-pen-to-square"
-				/>
-			</Button>
+			<p className="currentWeightText">Current Weight: {weight} kg</p>
 		</div>
 	);
 };
