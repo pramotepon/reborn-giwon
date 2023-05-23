ef56c77a7cf58cdff171613f4fee61aabf637bc6
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-05-23 15:10:49 +0700

    Fix form reset password completed

--- client/src/Screens/ResetPassScreen.jsx
+++ client/src/Screens/ResetPassScreen.jsx
@@ -1,89 +1,45 @@
 import React from "react";
 import LoginLayout from "../layout/LoginLayout/LoginLayout";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import IsLoadingComponent from "../components/IsLoadingComponent";
+import { Link } from "react-router-dom";
 import { useState } from "react";
 import axios from 'axios';
 import "../assets/css/login.css";
-import verifyToken from "../utils/verifyToken";
 import Swal from "sweetalert2";
 
 function ResetPassScreen() {
   const [email, setEmail] = useState();
-  const [height, setHeight] = useState();
-  const [currentweight, setcurrentWeight] = useState();
-  const [gender, setGender] = useState("male");
 
   const [isEmailValid, setEmailValid] = useState(true);
-  const [isHeightValid, setHeightValid] = useState(true);
-  const [isWeightValid, setWeightValid] = useState(true);
 
-  const navigate = useNavigate();
-
-  if (localStorage.getItem('user-reset-password')) {
-    return <Navigate to={'/newpass'} />
-  }
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleChangeEmail = (event) => {
     setEmail(event.target.value);
     console.log(event.target.value);
     setEmailValid(/\S+@\S+\.\S+/.test(event.target.value));
   };
-  const handleChangeHeight = (event) => {
-    setHeight(event.target.value);
-    setHeightValid(/^\d{1,4}$/.test(event.target.value));
-  };
-  const handleChangeCurrentWeight = (event) => {
-    setcurrentWeight(event.target.value);
-    setWeightValid(/^\d{1,4}$/.test(event.target.value));
-  };
-  const handleChangeGender = (event) => {
-    setGender(event.target.value);
-  };
-
-  const handleSave = (event) => {
-    event.preventDefault();
-
-    // Validate email
-    const isEmailValid = /\S+@\S+\.\S+/.test(email);
-    setShowEmailError(!isEmailValid);
-
-    if (!isEmailValid) {
-      console.log("Form validation failed");
-      return;
-    }
-
-    // Validate height
-    const isHeightValid = /^\d{1,4}$/.test(height);
-    setHeightValid(isHeightValid);
-
-    if (!isHeightValid) {
-      console.log("Form validation failed");
-      return;
-    }
-
-    // Validate weight
-    const isWeightValid = /^\d{1,4}$/.test(weight);
-    setWeightValid(isWeightValid);
-
-    if (!isWeightValid) {
-      console.log("Form validation failed");
-      return;
-    }
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setIsLoading(true);
     try {
       const formData = {
-        email,
-        height,
-        currentweight,
-        gender,
+        email
       };
 
       axios.post('/users/verify-before-reset-password/', formData).then(async (resolve) => {
         // const data = await verifyToken(resolve.data);
-        console.log(resolve.data);
+        Swal.fire({
+					title: "Successfully.",
+					text: resolve.data,
+					icon: "success",
+					confirmButtonText: "Ok!",
+				}).then((result) => {
+					if (result.isConfirmed) {
+						navigate("/login");
+					}
+				});
       }).catch((reject) => {
         const { message } = reject.response.data;
         Swal.fire({
@@ -92,6 +48,8 @@ function ResetPassScreen() {
           icon: 'error',
           confirmButtonText: 'Try'
         })
+      }).finally(()=>{
+        setIsLoading(false)
       });
     } catch (e) {
       console.log(e.message);
@@ -100,18 +58,15 @@ function ResetPassScreen() {
 
   const handleCancel = () => {
     setEmail("");
-    setHeight("");
-    setcurrentWeight("");
-    setGender("");
   };
 
   return (
     <LoginLayout>
+      {isLoading && <IsLoadingComponent />}
       <form action="" className="form-reset" onSubmit={handleSubmit}>
         <div className="reset-text">
           <h2 style={{ fontWeight: "bold" }}>Reset Password</h2>
         </div>
-
         <div className="reset-text">
           <label htmlFor="email">Email</label>
           <input
@@ -128,60 +83,6 @@ function ResetPassScreen() {
           )}
         </div>
 
-        <div
-          className="hw-reset"
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            gap: "10px",
-          }}
-        >
-          <div className="reset-text" style={{ flexBasis: "45%" }}>
-            <label htmlFor="Height">Height</label>
-            <input
-              type="text"
-              name="Height"
-              placeholder=""
-              className="input-regis weight-regis"
-              style={{ fontWeight: "bold" }}
-              onChange={handleChangeHeight}
-            />
-            {!isHeightValid && (
-              <div className=" position-absolute alert alert-danger translate-middle badge border border-light p-2"
-                style={{ left: "-67.5%", top: "50%", transform: "translate(-50%, -50%)" }}>Height must be a numeric value with a maximum of 4 characters</div>
-            )}
-          </div>
-
-          <div className="reset-text" style={{ flexBasis: "45%" }}>
-            <label htmlFor="CurrentWeight">Current Weight</label>
-            <input
-              type="text"
-              name="Weight"
-              placeholder=""
-              className="input-regis weight-regis"
-              style={{ fontWeight: "bold" }}
-              onChange={handleChangeCurrentWeight}
-            />
-            {!isWeightValid && (
-              <div className=" position-absolute alert alert-danger translate-middle badge border border-light p-2"
-                style={{ left: "-67.5%", top: "55%", transform: "translate(-50%, -50%)" }}>Weight must be a numeric value with a maximum of 4 characters</div>
-            )}
-          </div>
-        </div>
-
-        <div
-          className="reset-text"
-          style={{ fontSize: "18px", display: "flex", flexDirection: "row" }}
-        >
-          <label htmlFor="gender">Gender</label>
-          <input type="radio" name="gender" onChange={handleChangeGender} value={'male'} checked={gender === 'male'} />
-          Male
-          <input type="radio" name="gender" onChange={handleChangeGender} value={'female'} checked={gender === 'female'} />
-          Female
-          <input type="radio" name="gender" onChange={handleChangeGender} value={'prefer not to say'} checked={gender === 'prefer not to say'} />
-          Prefer not to say
-        </div>
-
         <div className="btn-regis">
           <div>
             <button
@@ -189,7 +90,7 @@ function ResetPassScreen() {
               style={{ marginRight: "26px", fontWeight: "bold" }}
             // onClick={handleSave}
             >
-              Set New Password
+              Send Email
             </button>
           </div>
 
